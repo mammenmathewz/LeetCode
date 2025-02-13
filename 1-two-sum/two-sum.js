1383da8d28a531007b30030de1a70526fd45b8de
Mammen Mathew <111522507+mammenmathewz@users.noreply.github.com>
2025-02-14 00:14:06 +0530

    Time: 5 ms (55.28%) | Memory: 52 MB (14.36%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    const map = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        let val = target - nums[i]
-        if (map.has(val)) {
-            return [i, map.get(val)]
-        } else {
-            map.set(nums[i], i)
+    let map = new Map()
+    for(let i =0 ; i<nums.length;i++){
+        let val = target-nums[i]
+        if(map.has(val)){
+            return [i,map.get(val)]
         }
+        map.set(nums[i],i)
     }
 };
